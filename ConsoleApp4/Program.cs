d552a627f4d9544a7c9cd82c972a59bf9efe7162
VLados <disledg@gmail.com>
2024-05-29 13:40:50 +0300

    new pull

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp4
+{
+    class Programm{
+        public void Main(string[] a){
+                
+        }
+    }
+}
